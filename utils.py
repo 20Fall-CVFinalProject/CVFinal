cf570e75baaea20c04c83e8ba776e40301859eda
WuDaqian <wudq1@shanghaitech.edu.cn>
2020-12-08 12:21:45 +0800

    utils update

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import cv2
 from PIL import Image
 import numpy as np
+from scipy import signal
 # data transform for image in svip data
 # svip_data_transforms = transforms.Compose([
 # 		# place holder
@@ -22,9 +23,35 @@ def get_gaze_direction_GT(head_x,head_y,gaze_x,gaze_y):
 	return get_direction(head_x,head_y,gaze_x,gaze_y)
 
 
-def get_heatmap_GT():
-	'''Get ground truth heatmap
+def get_heatmap_GT(height,width,gaze_x,gaze_y,kernlen=21, std=3):
+	'''Get ground truth heatmap or (51,9)
 	'''
+    gkern1d = signal.gaussian(kernlen, std=std).reshape(kernlen, 1)
+    kernel_map = np.outer(gkern1d, gkern1d)
+    k_size = kernel.shape[0] // 2
+    x, y = points
+    image_height, image_width = im_shape[:2]
+    x, y = int(round(image_width * x)), int(round(y * image_height))
+    x1, y1 = x - k_size, y - k_size
+    x2, y2 = x + k_size, y + k_size
+    h, w = shape
+    if x2 >= w:
+        w = x2 + 1
+    if y2 >= h:
+        h = y2 + 1
+    heatmap = np.zeros((h, w))
+    left, top, k_left, k_top = x1, y1, 0, 0
+    if x1 < 0:
+        left = 0
+        k_left = -x1
+    if y1 < 0:
+        top = 0
+        k_top = -y1
+
+    heatmap[top:y2+1, left:x2+1] = kernel[k_top:, k_left:]
+    return heatmap[0:shape[0], 0:shape[0]]
+
+	
 
 
 def get_direction_field(height,width,head_x,head_y,gaze_direction,gamma=1):
@@ -39,7 +66,7 @@ def get_direction_field(height,width,head_x,head_y,gaze_direction,gamma=1):
 			G = get_direction(head_x,head_y,idx_x,idx_y)
 			field[idx_y,idx_x] = max(G[0] * gaze_direction[0] + G[1] * gaze_direction[1],0) ** gamma
 	field = field * 255
-	return Image.fromarray(field.astype('float64'))
+	return Image.fromarray(field.astype('float64')).convert('RGB')
 
 def extract_gaze_point():
 	'''extract gaze point drom heatmap
